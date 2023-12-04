e60a0ad45c00f0699265cddfa3dc3427d3bc22e6
Ka0Ri <dtvu1707@gmail.com>
2023-12-04 18:18:28 +0900

    add bigger app

--- fastapi-quick-demo/main.py
+++ fastapi-quick-demo/main.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,3 +13,23 @@ async def root():
 async def square(num: int):
     result = num ** 2
     return {"squared": result}
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
